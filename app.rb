87da2d09daa0ae830f56c40fd119fc6a0a0edebb
David Salgado <david@digitalronin.com>
2020-07-18 11:39:29 +0100

    List/add users from the database

--- app.rb
+++ app.rb
@@ -2,7 +2,26 @@
 
 require "bundler/setup"
 require "sinatra"
+require "pg"
 
-get "/" do
-  "Hello world!"
+def connection
+  PG.connect(ENV.fetch("DATABASE_URL"))
+end
+
+def get_users
+  connection.exec( "SELECT * FROM users" )
+end
+
+def add_user(u)
+  addsql = %[ INSERT INTO users (first_name, last_name, password, email) VALUES ($1, $2, $3, $4) ]
+  connection.exec_params(addsql, [ u["first_name"], u["last_name"], u["password"], u["email"] ])
+end
+
+get "/users" do
+  erb :users, locals: { users: get_users }
+end
+
+post "/users" do
+  add_user(params)
+  redirect "/users"
 end
